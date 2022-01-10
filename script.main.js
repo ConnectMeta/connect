4897adcc5afff5914c9b47d3b131c8fb774351e7
Neil Shah <neildshah1013@gmail.com>
2022-01-09 20:07:06 -0500

    updated wallpeper

---
+++ script.main.js
@@ -0,0 +1,10 @@
+
+const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (e.matches) {
+    // change background-image of .bgimg1 to the dark theme
+    document.querySelector(".bgimg1").style.backgroundImage = "url('wallpaper-dark.jpg')";
+} else {
+    document.querySelector(".bgimg1").style.backgroundImage = "url('wallpaper.jpg')";
+  }
+});
